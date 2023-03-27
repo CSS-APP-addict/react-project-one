ae8dd6cc3e5596436f70229a51392eb48abfb567
CSS-APP-addict <css.app.project@gmail.com>
2023-03-27 18:39:21 +0800

    w

---
+++ components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import viteLogo from '/vite.svg'
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src="../vite.svg"/>
+        </nav>
+    )
+}
